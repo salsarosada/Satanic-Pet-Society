a3ac4ece8766298b7a7c154c45e844622839ecab
JuanJose082119 <32719927+JuanJose082119@users.noreply.github.com>
2023-05-20 19:01:35 -0500

    DesplazamientoSinAnimaciones

---
+++ Assets/Scripts/MovimientoPersonaje.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoPersonaje : MonoBehaviour
+{
+    public float speed = 5f;
+    public float minX = -5f; // Valor mínimo del eje X
+    public float maxX = 5f; // Valor máximo del eje X
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPosition.y = transform.position.y; // Mantener la posición en el eje Y
+            targetPosition.z = transform.position.z; // Mantener la posición en el eje Z
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minX, maxX); // Restringir al rango minX y maxX
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * speed);
+        }
+    }
+}

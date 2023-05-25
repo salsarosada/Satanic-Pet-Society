681cca0df185aaa87cbef7c3c13992e40d4bfed5
JuanJose082119 <32719927+JuanJose082119@users.noreply.github.com>
2023-05-24 22:36:16 -0500

    Pezca

---
+++ Assets/Scripts/SistemaPesca.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SistemaPesca : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject[] fishPrefabs;
+    public Transform fishingRod;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartFishing();
+        }
+    }
+
+    private void StartFishing()
+    {
+        // Obtener un índice aleatorio para seleccionar un pez del array
+        int randomFishIndex = Random.Range(0, fishPrefabs.Length);
+
+        // Instanciar un objeto de pez y anclarlo a la caña de pescar
+        GameObject fish = Instantiate(fishPrefabs[randomFishIndex], fishingRod);
+        fish.transform.localPosition = Vector3.zero;
+
+        // Iniciar la animación de pesca
+        animator.SetTrigger("Fishing");
+    }
+
+    public void CaptureFish()
+    {
+        // Ejecutar la animación de pesca en reversa
+        animator.SetTrigger("ReelIn");
+
+        // Obtener el pez anclado a la caña de pescar
+        GameObject fish = fishingRod.GetChild(0).gameObject;
+
+        // Destruir el pez después de 2 segundos
+        Destroy(fish, 2f);
+    }
+}

df52a2218ce83ba537adab3dfd6f509bc0b7495a
JuanJose082119 <32719927+JuanJose082119@users.noreply.github.com>
2023-05-24 20:56:33 -0500

    Limpieza

---
+++ Assets/Scripts/SistemaLimpieza.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SistemaLimpieza : MonoBehaviour
+{
+    public Image limpiezaBar;
+    public float nivelLimpiezaMaximo = 100f;
+    public float nivelLimpiezaMinimo = 30f;
+    public float velocidadDecrementoLimpieza = 1f;
+
+    private float nivelLimpiezaActual;
+
+    private void Start()
+    {
+        nivelLimpiezaActual = nivelLimpiezaMaximo;
+    }
+
+    private void Update()
+    {
+        // Disminuir gradualmente el nivel de limpieza con el tiempo
+        nivelLimpiezaActual -= velocidadDecrementoLimpieza * Time.deltaTime;
+
+        // Actualizar la barra de limpieza
+        float fillAmount = nivelLimpiezaActual / nivelLimpiezaMaximo;
+        limpiezaBar.fillAmount = fillAmount;
+
+        // Verificar si el nivel de limpieza está por debajo del mínimo
+        if (nivelLimpiezaActual < nivelLimpiezaMinimo)
+        {
+            // Cambiar el sprite a uno sucio
+            // Puedes utilizar un componente SpriteRenderer en el objeto de la mascota
+            // y asignarle el sprite sucio adecuado
+            // Ejemplo: GetComponent<SpriteRenderer>().sprite = spriteSucio;
+        }
+    }
+}
